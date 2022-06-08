f8a8d98d26fda5ace99986d8f06e68271f685cd7
Adarsh275 <adarshaman2000@gmail.com>
2022-06-08 13:49:29 +0530

    Creating a circle with different color of stars using turtle module

---
+++ Python/Circle_With_Stars.py
@@ -0,0 +1,18 @@
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('Black')
+t.speed(0)
+n = 36
+h = 0
+
+for i in range(80):
+    c = colorsys.hsv_to_rgb(h,1,0.8)
+    h+= 1/n
+    t.color(c)
+    t.left(5)
+    for j in range (5):
+        t.forward(250)
+        t.left(144)
